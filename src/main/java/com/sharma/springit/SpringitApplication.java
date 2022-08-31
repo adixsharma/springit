59d72621b05c0979505cc7f4c61626f7a7dd379a
Aditya Sharma <adi@Adityas-MacBook-Pro-2.local>
2022-08-31 12:15:41 -0700

    Created our entities and refactored using Project Lombok

--- src/main/java/com/sharma/springit/SpringitApplication.java
+++ src/main/java/com/sharma/springit/SpringitApplication.java
@@ -1,13 +1,55 @@
 package com.sharma.springit;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpringitApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
+
+
+
+
+//    @Bean
+//    CommandLineRunner runner(){
+//        return args -> {
+//            log.error("CommandLineRunner.run()");
+//            log.warn( "CommandLineRunner.run()");
+//            log.info("CommandLineRunner.run()");
+//            log.debug("CommandLineRunner.run()");
+//            log.trace("CommandLineRunner.run()");
+//        };
+//    }
+
+
+
+
+
+//
+//    @Bean
+//    CommandLineRunner runner(){
+//        return args -> {
+//            System.out.println("Printing out all the bean names in the application context.");
+//            System.out.println("------------------------------------------------------------");
+//            String[] beans = applicationContext.getBeanDefinitionNames();
+//
+//            Arrays.sort(beans);
+//            for (String bean: beans){
+//                System.out.println(bean);
+//            }
+//        };
+//    }
+//
+
 
 }
---
+++ src/main/java/com/sharma/springit/domain/Link.java
@@ -0,0 +1,23 @@
+package com.sharma.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private String url;
+}
